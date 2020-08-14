e9314fca1201f6b218e09b6e049a5ec6e4d1753f
Renata Hodovan <reni@inf.u-szeged.hu>
2020-08-14 15:59:23 +0200

    Adapt the value of max_depth if needed
    Although the generated fuzzer is aware of the depth of the minimal
    derivation of a certain rule and hence it's able to ensure finishing
    the generation in a predefined depth, however this only works if there
    are no inline predicates in the grammar. In such cases, it can navigate
    into unresolvable situations where the available derivatations are
    disabled by the predicates. The current patch solves such situations
    by temporarily raising the max depth to the minimal required value and
    it will finish the generation with choosing the shortest possible
    derivation path.

--- grammarinator/runtime/generator.py
+++ grammarinator/runtime/generator.py
@@ -1,13 +1,17 @@
-# Copyright (c) 2017-2021 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2022 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
+import logging
+
 from math import inf
 
 from .default_model import DefaultModel
+
+logger = logging.getLogger(__name__)
 
 
 def depthcontrol(fn):
@@ -37,3 +41,11 @@ class Generator(object):
     def _exit_rule(self, node):
         for listener in reversed(self._listeners):
             listener.exit_rule(node)
+
+    def _filter_options(self, depths, weights):
+        available_options = [w if depths[i] <= self._max_depth else 0 for i, w in enumerate(weights)]
+        if sum(available_options) > 0:
+            return available_options, self._max_depth
+        max_depth = min(depths[i] if w > 0 else inf for i, w in enumerate(weights))
+        logger.debug('max_depth must be temporarily set from %s to %s', self._max_depth, max_depth)
+        return [w if depths[i] <= max_depth else 0 for i, w in enumerate(weights)], max_depth
